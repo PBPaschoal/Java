6255987b4514573b8b625564e21548df37eeb34a
PBPaschoal <programming.paschoal@gmail.com>
2023-06-21 21:51:52 -0300

    Continuando as aulas da Dio e iniciando a revisao das aulas da Udemy com o instrutor Nelio Alves, curso avançado de Java POO

--- Dio/Formação Java Developer/Primeira etapa - Fundamentos da Plataforma Java/Curso 03 - Aprendendo a Sintaxe Java/Aula 01 e 02 - Tipos e Variaveis/tipos-variaveis/src/TiposVariaveis.java
+++ Dio/Formação Java Developer/Primeira etapa - Fundamentos da Plataforma Java/Curso 03 - Aprendendo a Sintaxe Java/Aula 01 e 02 - Tipos e Variaveis/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
-public class App {
+public class TiposVariaveis {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.err.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
---
+++ Udemy/Java COMPLETO 2023 POO/src/Main.java
@@ -0,0 +1,14 @@
+// Revisando: 21-06-2023
+// CURSO: Java COMPLETO 2023 Programação Orientada a Objetos + Projetos
+// Plataforma: Udemy
+// Aula 17 Primeiro Programa
+// Observação: Apesar do curso ser todo em Português, irei forcar nas nomenclaturas e textos em inglês.
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+	}
+
+}
