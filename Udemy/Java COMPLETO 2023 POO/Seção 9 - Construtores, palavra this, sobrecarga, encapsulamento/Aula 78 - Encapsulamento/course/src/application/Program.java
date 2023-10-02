f158bca29d201dfc6d3829c786cc667920556911
Paulo Bruno Paschoal <programming.paschoal@gmail.com>
2023-10-02 14:33:07 -0300

    Aula 78 Encapsulamento - E organizando as pastas

---
+++ Udemy/Java COMPLETO 2023 POO/Seção 9 - Construtores, palavra this, sobrecarga, encapsulamento/Aula 78 - Encapsulamento/course/src/application/Program.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner keyboard = new Scanner(System.in);
+		
+		Product p = new Product();
+		
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		String name = keyboard.nextLine();
+		System.out.print("Price: ");
+		double price = keyboard.nextDouble();
+		Product product = new Product(name, price);
+		
+		product.setName("Computer");
+		System.out.println("Updated name: " + product.getName());
+		product.setPrice(1200.00);
+		System.out.println("Updated price: " + product.getPrice());
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = keyboard.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = keyboard.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		keyboard.close();
+	}
+}
