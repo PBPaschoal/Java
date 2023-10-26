66b5ae35a74063fe3b476f73bcb13958274830d5
Paulo Bruno Paschoal <38387802+PBPaschoal@users.noreply.github.com>
2023-10-26 10:02:54 -0300

    Calculadora básica SomaSwing
    Atualizando demais exemplos

--- Curso em vídeo/Java Básico/Aula 6 – Tipos Primitivos e Manipulação de Dados/TestePrimitivo/src/testeprimitivo/TestePrimitivo.java
+++ Curso em vídeo/Java Básico/Aula 6 – Tipos Primitivos e Manipulação de Dados/TestePrimitivo/src/testeprimitivo/TestePrimitivo.java
@@ -7,6 +7,13 @@ public class TestePrimitivo {
     public static void main(String[] args) {
         int idade = 30;
         String valor = Integer.toString(idade);
+        String valor2 = "50";
+        int idade2 = Integer.parseInt(valor2);
+        String valor3 = "50.6";
+        float idade3 = Float.parseFloat(valor3);
+        
         System.out.println(valor);
+        System.out.println(idade2);
+        System.out.println(idade3);
     }
 }
