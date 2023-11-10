0a4ead19d1e58406ffee698630c63335c8ef04ae
Paulo Bruno <programming.paschoal@gmail.com>
2023-11-10 15:26:39 -0300

    Novos dados

--- Projetos/Meus Projetos/Simulador Emprestimo Consignado/simulador_consignado_v6/src/financas/Contrato.java
+++ Projetos/Meus Projetos/Simulador Emprestimo Consignado/simulador_consignado_v6/src/financas/Contrato.java
@@ -9,11 +9,13 @@ public class Contrato {
 	private double prazo;
 	private double valorLiquido;
 	private double valorSaldoDevedor;
+	private double fator;
 	private String nomeCliente;
 	private String cpfCliente;
 	private byte idadeCliente;
 	private byte prazoCliente;
 	private char sexoCliente;
+	private byte operacao;
 	
 	public Contrato(Scanner keyboard, double parcela, double prazo, double valorLiquido, double valorSaldoDevedor) {
 		this.keyboard = keyboard;
@@ -40,7 +42,7 @@ public class Contrato {
 	}
 	
 	public void DadosDoCliente() {
-		System.out.println("\nVOCE SELECIOU CONTRATO NOVO!");
+		System.out.println("\n::::::::::::::::::::::::");
 		System.out.println("Informe os dados abaixo:");
 		System.out.print("NOME DO CLIENTE: ");
 		
@@ -55,6 +57,7 @@ public class Contrato {
 	}
 	
 	public void ContratoNovo() {
+		System.out.println("\nVOCE SELECIOU CONTRATO NOVO!");
 		System.out.println("DESEJA SIMULAR PELO VALOR DA PARCELA OU PELO VALOR DO CONTRATO?");
 		System.out.println("[1] PARCELA");
 		System.out.println("[2] CONTRATO");
@@ -66,13 +69,43 @@ public class Contrato {
 			System.out.print("VALOR LIBERADO: R$");
 		}
 		
+		operacao = 1;
+		
 	}
 	
 	public void ContratoDeRefinanciamento() {
+		System.out.println("\nVOCE SELECIOU CONTRATO DE REFINANCIAMENTO!");
 		System.out.print("PARCELA: R$");
 		System.out.print("SALDO DEVEDOR: R$");
+		operacao = 2;
 	}
 	
+	public double CalcularContratoNovo() {
+		return valorLiquido = parcela * fator;
+	}
 	
+	public double CalcularContratoRefinanciamento() {
+		return valorLiquido = parcela * fator - valorSaldoDevedor;
+	}
+	
+	public void Resultado() {
+		System.out.println("NOME DO CLIENTE: " + nomeCliente);
+		System.out.println("CPF DO CLIENTE: " + cpfCliente);
+		System.out.println("IDADE DO CLIENTE: " + idadeCliente);
+		System.out.println("SEXO DO CLIENTE: " + sexoCliente);
+		System.out.println("PRAZO DO CONTRATO: " + prazo);
+		System.out.println(".......................................");
+		if(operacao == 1) {
+			System.out.println("VALOR LIBERADO: R$" + CalcularContratoNovo());
+		}
+		
+		else {
+			
+		}
+		
+		System.out.println("PORCENTAGEM DA COMISSAO: ");
+		System.out.println("VALOR DA COMISSÃO DO CORRETOR: ");
+		
+	}
 
 }
