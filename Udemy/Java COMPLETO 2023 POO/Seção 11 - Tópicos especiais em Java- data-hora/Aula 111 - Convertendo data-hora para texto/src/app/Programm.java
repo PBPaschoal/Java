d94fd06cb2327c7f21b7f67a48fb341320e4086b
PBPaschoal <programming.paschoal@gmail.com>
2023-11-27 10:36:08 -0300

    Convertendo data-hora para texto

---
+++ Udemy/Java COMPLETO 2023 POO/Seção 11 - Tópicos especiais em Java- data-hora/Aula 111 - Convertendo data-hora para texto/src/app/Programm.java
@@ -0,0 +1,35 @@
+package app;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class Programm {
+
+	public static void main(String[] args) {
+		LocalDate d04 = LocalDate.parse("2023-11-27");
+		LocalDateTime d05 = LocalDateTime.parse("2023-07-27T01:30:26");
+		Instant d06 = Instant.parse("2023-07-27T01:30:26Z");
+
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+		DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
+		DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
+
+		System.out.println("d04 = " + d04.format(fmt1));
+		System.out.println("d04 = " + fmt1.format(d04));
+		System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		System.out.println("d05 = " + d05.format(fmt1));
+		System.out.println("d05 = " + d05.format(fmt2));
+		System.out.println("d05 = " + d05.format(fmt4));
+		System.out.println("d06 = " + d06);
+		System.out.println("d06 = " + fmt3.format(d06));
+		System.out.println("d06 = " + fmt5.format(d06));
+		System.out.println("d06 = " + d06.toString());
+
+	}
+
+}
