40aab06e31d5dff59771a32d16480c745e46844d
PBPaschoal <programming.paschoal@gmail.com>
2023-11-27 09:22:17 -0300

    Instanciando data-hora em Java

--- Udemy/Java COMPLETO 2023 POO/Seção 11 - Tópicos especiais em Java- data-hora/Aula 110 - Instanciando data-hora em Java/src/app/Program.java
+++ Udemy/Java COMPLETO 2023 POO/Seção 11 - Tópicos especiais em Java- data-hora/Aula 110 - Instanciando data-hora em Java/src/app/Program.java
@@ -10,6 +10,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		
 		LocalDate d01 = LocalDate.now();
 		LocalDateTime d02 = LocalDateTime.now();
@@ -19,6 +20,9 @@ public class Program {
 		Instant d06 = Instant.parse("2023-11-26T01:30:26Z");
 		Instant d07 = Instant.parse("2023-11-26T01:30:26-03:00");
 		LocalDate d08 = LocalDate.parse("26/11/2023", fmt1);
+		LocalDateTime d09 = LocalDateTime.parse("20/07/2022 01:30", fmt2);
+		LocalDate d10 = LocalDate.of(2023, 11, 27);
+		LocalDateTim d11 = LocalDateTime.of(2023, 11, 27, 1, 30);
 
 		System.out.println("d01 = " + d01);
 		System.out.println("d02 = " + d02);
@@ -28,6 +32,9 @@ public class Program {
 		System.out.println("d06 = " + d06);
 		System.out.println("d07 = " + d07);
 		System.out.println("d08 = " + d08);
+		System.out.println("d09 = " + d09);
+		System.out.println("d010 = " + d010);
+		System.out.println("d011 = " + d011);
 	}
 
 }
