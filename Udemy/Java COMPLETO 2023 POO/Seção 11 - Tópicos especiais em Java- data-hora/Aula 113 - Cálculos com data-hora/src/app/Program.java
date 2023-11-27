903986c44a8790eed879250a0d42d5343d35c9b4
PBPaschoal <programming.paschoal@gmail.com>
2023-11-27 11:30:47 -0300

    Cálculos com data-hora

---
+++ Udemy/Java COMPLETO 2023 POO/Seção 11 - Tópicos especiais em Java- data-hora/Aula 113 - Cálculos com data-hora/src/app/Program.java
@@ -0,0 +1,46 @@
+package app;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Program {
+
+	public static void main(String[] args) {
+		LocalDate d04 = LocalDate.parse("2023-11-27");
+		LocalDateTime d05 = LocalDateTime.parse("2023-11-27T01:30:26");
+		Instant d06 = Instant.parse("2023-11-27T01:30:26Z");
+
+		LocalDate pastWeekLocalDate = d04.minusDays(7);
+		LocalDate nextWeekLocalDate = d04.plusDays(7);
+
+		System.out.println("pastWeekLocalDate = " + pastWeekLocalDate);
+		System.out.println("nextWeekLocalDate = " + nextWeekLocalDate);
+		
+		LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
+		LocalDateTime nextWeekLocalDateTime = d05.plusDays(7);
+
+		System.out.println("pastWeekLocalDateTime = " + pastWeekLocalDateTime);
+		System.out.println("nextWeekLocalDateTime = " + nextWeekLocalDateTime);
+		
+		Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+		Instant nextWeekInstant = d06.plus(7, ChronoUnit.DAYS);
+
+		System.out.println("pastWeekInstant = " + pastWeekInstant);
+		System.out.println("nextWeekInstant = " + nextWeekInstant);
+		
+		Duration t1 = Duration.between(pastWeekLocalDate.atTime(0, 0), d04.atTime(0, 0)); // ou .atStartOfDay(),
+		Duration t2 = Duration.between(pastWeekLocalDateTime, d05);
+		Duration t3 = Duration.between(pastWeekInstant, d06);
+		Duration t4 = Duration.between(d06, pastWeekInstant);
+
+		System.out.println("t1 dias = " + t1.toDays());
+		System.out.println("t2 dias = " + t2.toDays());
+		System.out.println("t3 dias = " + t3.toDays());
+		System.out.println("t4 dias = " + t4.toDays());
+
+	}
+
+}
