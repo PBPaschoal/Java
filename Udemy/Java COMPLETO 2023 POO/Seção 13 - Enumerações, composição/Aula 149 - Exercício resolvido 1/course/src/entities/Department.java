59edae64ff58afc48014c3ddad7350b363ba9466
PBPaschoal <programming.paschoal@gmail.com>
2023-12-20 15:24:54 -0300

    Exercicio parte 1

---
+++ Udemy/Java COMPLETO 2023 POO/Seção 13 - Enumerações, composição/Aula 149 - Exercício resolvido 1/course/src/entities/Department.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Department {
+
+	private String name;
+
+	public Department(){
+	}
+
+	public Department(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String setName() {
+		return name;
+	}
+
+}
