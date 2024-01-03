066a3793f9bc8cbfc31cd9dbff84075b9f451912
Paulo Bruno Paschoal <programming.paschoal@gmail.com>
2024-01-03 15:17:11 -0300

    Aula concluida

--- Udemy/Java COMPLETO 2023 POO/Seção 14 - Herança e polimorfismo/Aula 156 e 157 - Herança, Upcasting e downcasting/src/application/Program.java
+++ Udemy/Java COMPLETO 2023 POO/Seção 14 - Herança e polimorfismo/Aula 156 e 157 - Herança, Upcasting e downcasting/src/application/Program.java
@@ -16,6 +16,23 @@ public class Program {
 		Account acc1 = bacc;
 		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
 		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+		
+		// DOWNCASTING
+		
+		BusinessAccount acc4 = (BusinessAccount)acc2;
+		acc4.loan(100.0);
+		
+		// BusinessAccount acc5 = (BusinessAccount)acc3;
+		if(acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+			acc5.loan(200.0);
+			System.out.println("Loan!");
+		}
+		if(acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
 
 	}
 
