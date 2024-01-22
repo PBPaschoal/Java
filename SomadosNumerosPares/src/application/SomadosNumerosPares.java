95a093d07516679e984bb4645a110ec9a029d80c
PBPaschoal <programming.paschoal@gmail.com>
2024-01-22 16:09:43 -0300

    A soma de todos os numeros pares de 1 até N

---
+++ SomadosNumerosPares/src/application/SomadosNumerosPares.java
@@ -0,0 +1,32 @@
+/* Soma dos Números Pares
+ * Escreva um programa em Java que solicita ao usuário para inserir um número inteiro positivo. O programa deve calcular e exibir a soma de 
+ * todos os números pares de 1 até o número fornecido pelo usuário
+ */
+
+package application;
+
+import java.util.Scanner;
+
+public class SomadosNumerosPares {
+	
+	public static void main(String[] args) {
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		int soma = 0;
+		
+		System.out.print("Informe um numero: ");
+		int numero = keyboard.nextInt();
+		
+		for(int i = 0; i <= numero; i++) {
+			if(i % 2 == 0) {
+				soma += i;
+			}
+		}
+		
+		System.out.println("A soma de todos os números pares de 1 até " + numero + " = : " + soma);
+		
+		keyboard.close();
+	}
+
+}
