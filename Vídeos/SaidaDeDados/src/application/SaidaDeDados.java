fed49a55857cd8a2bd1c246cd256f32ae81b66e0
PBPaschoal <programming.paschoal@gmail.com>
2024-04-23 17:29:34 -0300

    Conteudo video

---
+++ Vídeos/SaidaDeDados/src/application/SaidaDeDados.java
@@ -0,0 +1,11 @@
+package application;
+
+public class SaidaDeDados {
+	
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo!");
+		System.out.print("Teste");
+		System.out.println("Teste 2");
+	}
+
+}
