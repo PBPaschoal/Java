da1ee65d69bfefa1736f485fe4a25350dafd6a99
Paulo Bruno Paschoal <programming.paschoal@gmail.com>
2024-06-20 20:13:43 -0300

    Atualizações

--- Projetos - Spring Boot/SimpleWebApp/src/main/java/com/example/demo/controller/LoginController.java
+++ Projetos - Spring Boot/SimpleWebApp/src/main/java/com/example/demo/controller/LoginController.java
@@ -6,23 +6,45 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class LoginController {
 
+    private Map<String, String> userDatabase = new HashMap<>(); // Simples banco de dados em memória
+
     @GetMapping("/login")
-    public String showLoginForm() {
+    public String showLoginForm(Model model) {
+        model.addAttribute("errorMessage", "");
         return "login";
     }
 
     @PostMapping("/login")
     public String processLogin(@RequestParam String username, @RequestParam String password, Model model) {
-        // Aqui você pode adicionar lógica de autenticação
-        if ("user".equals(username) && "password".equals(password)) {
-            model.addAttribute("message", "Login successful!");
-            return "index"; // Redireciona para a página inicial ou outra página de sucesso
+        if (userDatabase.containsKey(username) && userDatabase.get(username).equals(password)) {
+            model.addAttribute("message", "Login bem-sucedido!");
+            return "index";
         } else {
-            model.addAttribute("message", "Invalid username or password.");
-            return "login"; // Volta para a página de login com a mensagem de erro
+            model.addAttribute("errorMessage", "Nome de usuário ou senha inválidos.");
+            return "login";
         }
+    }
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+        model.addAttribute("message", "");
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegister(@RequestParam String username, @RequestParam String password, Model model) {
+        if (userDatabase.containsKey(username)) {
+            model.addAttribute("message", "O nome de usuário já existe.");
+        } else {
+            userDatabase.put(username, password);
+            model.addAttribute("message", "Registrado com sucesso!");
+        }
+        return "register";
     }
 }
