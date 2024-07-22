b3fb48435d37b6069786e0147ce08bab3bd48c4b
Paulo Bruno Paschoal <programming.paschoal@gmail.com>
2024-07-22 19:06:14 -0300

    Ajustando erro de login; adicionando página de mensagem de erro; testando

--- Projetos - Spring Boot/SimpleWebApp/src/main/java/com/example/demo/controller/UserController.java
+++ Projetos - Spring Boot/SimpleWebApp/src/main/java/com/example/demo/controller/UserController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
@@ -15,13 +18,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/register")
+    @GetMapping("/user/register")
     public String showRegistrationForm(Model model) {
         model.addAttribute("message", "");
         return "register";
     }
 
-    @PostMapping("/register")
+    @PostMapping("/user/register")
     public String registerUser(@RequestParam String fullname,
                                @RequestParam String username,
                                @RequestParam String password,
@@ -41,13 +44,13 @@ public class UserController {
         }
     }
 
-    @GetMapping("/login")
+    @GetMapping("/user/login")
     public String showLoginForm(Model model) {
         model.addAttribute("errorMessage", "");
         return "login";
     }
 
-    @PostMapping("/login")
+    @PostMapping("/user/login")
     public String loginUser(@RequestParam String username,
                             @RequestParam String password,
                             Model model) {
@@ -56,10 +59,20 @@ public class UserController {
         if (user != null && user.getPassword().equals(password)) {
             model.addAttribute("fullname", user.getFullname());
             model.addAttribute("message", "Login bem-sucedido!");
-            return "welcome"; // Redireciona para a página de boas-vindas
+            return "welcome"; // Certifique-se de retornar "welcome"
         } else {
             model.addAttribute("errorMessage", "Nome de usuário ou senha incorretos.");
             return "login";
+        }
+    }
+    
+    @Controller
+    public class CustomErrorController implements ErrorController {
+
+        @RequestMapping("/error")
+        public String handleError() {
+            // Redireciona para a página de erro personalizada
+            return "error";
         }
     }
 }
