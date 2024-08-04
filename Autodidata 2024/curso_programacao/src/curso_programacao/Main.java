d16bde5b586e71a3b5e67e0586cc58e541ed8f65
Paulo Bruno Paschoal <programming.paschoal@gmail.com>
2024-08-04 19:11:56 -0300

    As três operações básicas de programação; Saída de dados em Java (System.out.print; System.out.println; System.out.prif; %d; %f; %s; %n; Locale; Como concatenar vários elementos em um mesmo comando de escrita; Exemplos.

--- Autodidata 2024/curso_programacao/src/curso_programacao/Main.java
+++ Autodidata 2024/curso_programacao/src/curso_programacao/Main.java
@@ -1,11 +1,23 @@
-// Primeiro programa
 package curso_programacao;
+
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Olá mundo!");
+		Locale.setDefault(Locale.US);
+		int y = 32;
+		double x = 10.35784;
+		String nome = "Maria";
+		int idade = 31;
+		double renda = 4000.0;
 
+		System.out.println("Olá mundo!");
+		System.out.println(y);
+		System.out.printf("%.2f%n", x);
+		System.out.println("RESULTADO = " + x + " METROS");
+		System.out.printf("RESULTADO = %.2f metros%n", x);
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
 	}
 
 }
