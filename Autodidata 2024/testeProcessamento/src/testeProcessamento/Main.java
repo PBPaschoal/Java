94a55022b0743ad7f247a97ff559efcbec8c14a3
Paulo Bruno Paschoal <programming.paschoal@gmail.com>
2024-08-05 20:33:55 -0300

    Processamento de dados em Java, Casting

---
+++ Autodidata 2024/testeProcessamento/src/testeProcessamento/Main.java
@@ -0,0 +1,30 @@
+package testeProcessamento;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x, y, c, a, g, p;
+		double d, b, B, h, area, resultado, j;
+		x = 5;
+		y = 2 * x;
+		c = 5;
+		d = 2 * c;
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+		a = 5;
+		g = 2;
+		area = (b + B) / 2.0 * h;
+		resultado = (double) a / g;
+		j = 5.0;
+		p = (int) j;
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(area);
+		System.out.println(resultado);
+		System.out.println(p);
+	}
+
+}
