0e89b108fea26dbaf20ae55ef09baf94a127afe7
Paulo Paschoal <programming.paschoal@gmail.com>
2024-10-09 21:00:15 -0300

    Aula 67 e 68 - Exemplo; Object e toString

--- Revisando Setembro 2024/aula68/src/application/Program.java
+++ Revisando Setembro 2024/aula68/src/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
 		System.out.print("Quantity in stock: ");
 		product.quantity = keyboard.nextInt();
 		
-		System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+		System.out.println(product);
 		
 		keyboard.close();
 	}
