b4b533a00ce33ef14d6b7bbfda6a9da4cc91835c
Paulo Paschoal <programming.paschoal@gmail.com>
2024-11-11 20:20:53 -0300

    Vetores - Parte 02

---
+++ Revisando Setembro 2024/exerciciosVetores01/src/applicatiob/Program.java
@@ -0,0 +1,32 @@
+//Faça um programa que leia um número inteiro Positivo N (máximo = 10) e depois N números inteiros e armazene-os em 
+//um vetor. Em seguida, mostrar na tela todos os números negativos lidos.
+//Exemplo:
+//Quantos numeros você vai digitar? 6
+//Digite um numero: 8
+//Digite um numero: -2
+//Digite um numero: 9
+//Digite um numero: 10
+//Digite um numero: -3
+//Digite um numero: -7
+//NUMEROS NEGATIVOS:
+//-2
+//-3
+//-7
+
+package applicatiob;
+
+import java.util.Scanner;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("Quantos numeros voce vai digitar? ");
+		int quantidade = keyboard.nextInt();
+		int vect
+		
+		keyboard.close();
+	}
+
+}
