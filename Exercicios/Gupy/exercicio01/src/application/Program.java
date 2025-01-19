51172ed63477dfb21330ddb3cb222d1729aa1ea2
Paulo Paschoal <programming.paschoal@gmail.com>
2025-01-18 22:15:23 -0300

    Exercicio 01

---
+++ Exercicios/Gupy/exercicio01/src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Program {
+	public static void main(String[] args) {
+        int INDICE = 13, SOMA = 0, K = 0;
+
+        while (K < INDICE) {
+            K = K + 1;
+            SOMA = SOMA + K;
+        }
+
+        System.out.println(SOMA);
+    }
+}
