43eec83df8b109c122adc2dcd31a6ad340d110f2
Paulo Bruno <programming.paschoal@gmail.com>
2025-03-09 19:48:39 -0300

    Exercicio resolvido 01 - Aula 123

---
+++ Revisando Março 2025/aula123exercicio1/src/application/Program.java
@@ -0,0 +1,13 @@
+// Parte 1 e parte 2 - Aula 123 e 124
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ Revisando Março 2025/aula123exercicio1/src/entities/Department.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class Department {
+	
+	private String name;
+	
+	public Department() {
+	}
+	
+	public Department(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+}
